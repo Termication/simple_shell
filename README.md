08d39fefba54c86e3698c361a64b128a3e2b18bc
Terminal <terminalkarabo@gmail.com>
2024-02-23 06:32:35 -0800

    main headerfile

---
+++ README.md
@@ -0,0 +1,1 @@
+simple shell implementation written in C programming language. It provides a basic command-line interface where users can execute commands, navigate directories, and manage processes.
---
+++ shell_main.h
@@ -0,0 +1,21 @@
+#ifndef _SHELL_MAIN_H_
+#define _SHELL_MAIN_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <signal.h>
+
+
+#define MAIN_BUF_SIZE 1024
+#define READABLE_BUF_SIZE 512
+
+
