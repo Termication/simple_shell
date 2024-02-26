ee3114957df62018ca718ce2e3399b8f83e3dbb0
Terminal <terminalkarabo@gmail.com>
2024-02-26 08:59:39 -0800

    main c update

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 int main(void)
 {
 	char cmd[100];	/* Buffer to store user input */
+	char *args[MAX_ARGS];
+	int num_args;
 
 	/* Infinite loop to continuously prompt for input */
 	while (1)
@@ -16,7 +18,12 @@ int main(void)
 		dis_prompt();
 		get_user_input(cmd, sizeof(cmd));
 		cmd_execute(cmd);
+	
 	}
+
+	num_args = tokenize_command(command_line, args);
+
+
 
 	return (0);
 }
